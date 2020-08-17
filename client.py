53ff96ac0fb7606d7770d349b6d27eaebc6b7014
kuzaster <as-kuz@mail.ru>
2020-08-17 17:06:16 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+
+
+def read_sock():    # Функция для считывания сообщений с сервера
+    while True:
+        data = sock.recv(1024).decode('utf-8')
+        print(f'\n{data}')
+
+
+server = 'localhost', 9090
+nickname = input('Enter your name: ')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 0))
+sock.sendto(f'{nickname} connected to chat'.encode('utf-8'), server)
+
+thread = threading.Thread(target=read_sock)     # Создает потом и запускается в нем функция read_sock, чтобы прием
+thread.start()                                  # сообщений приходил по мере их отправки (в постоянном потоке)
+
+while True:
+    message = input('Enter your message:\n')
+    sock.sendto(f'[{nickname}] {message}'.encode('utf-8'), server)
---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 9090))
+members = []    # Список участников в чате. Хранится IP и PORT пользователя
+
+print('Start Server')
+
+while True:
+    data, addr = sock.recvfrom(1024)    # data - сообщение от пользователя. addr - адрес пользователя
+    if addr not in members:     # Если пользователя нет в списке, то добавлет его туда
+        members.append(addr)    # и выводит сообщение о подключении пользователя с его данными
+        print(f'{addr[0]}, {addr[1]} connected')
+    if len(members) < 2:    # Предупреждает, если в чате только один пользователь
+        sock.sendto("Please, wait another user, you are the only at this chat".encode('utf-8'), addr)
+    for member in members:
+        if member == addr:      # Не отсылает сообщение самому автору
+            continue
+        sock.sendto(data, member)
+
